15786cb8f17ca3a7a446607e399e5fce64c641dd
sebmuel <mueller.sebastian117@protonmail.com>
2022-09-01 21:11:15 +0200

    Projektdateien hinzufügen.

---
+++ Umbr/App_Plugins/ActionDashboard/actionDashboard.controller.js
@@ -0,0 +1,12 @@
+﻿angular.module("umbraco").controller("ActionDashboardController", function ($scope, contentResource) {
+
+    var vm = this;
+
+    contentResource.getById(1151).then(function (data) {
+        var nodes = data.variants;
+
+        console.log(nodes[0].name)
+
+        vm.Partner = nodes[0].name;
+    });
+});
